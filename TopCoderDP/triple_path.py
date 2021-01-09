3f0672c32f9b904c9a75825812ab1f15c4e9fa29
riccardompesce <riccardompesce@gmail.com>
2021-01-09 22:10:07 +0100

    Finsihed problem the most unelegant way

--- TopCoderDP/triple_path.py
+++ TopCoderDP/triple_path.py
@@ -1,30 +1,50 @@
 def triple_path_finder(grid):
     path = []
+    items = []
+    after_path = []
+    three_paths = []
 
     for i in range(len(grid)):
         path += [[]]
+        items += [[]]
         for j in range(len(grid[i])):
             path[i] += [0]
+            items[i] += [[]]
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if i == 0 and j == 0:
-                path[i][j] = grid[i][j]
-            elif i == 0:
-                path[i][j] = grid[i][j] + path[i][j - 1]
-            elif j == 0:
-                path[i][j] = grid[i][j] + path[i - 1][j]
-            elif i == len(grid) - 1 and j == 0:
-                path[i][j] = grid[i][j] + path[i - 1][j]
-            elif i == 0 and j == len(grid[i]) - 1:
-                path[i][j] = grid[i][j] + path[i][j - 1]
-            else:
-                if path[i - 1][j] > path[i][j - 1]:
-                    path[i][j] = grid[i][j] + path[i - 1][j]
-                else:
+    for n in range(3):
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if i == 0 and j == 0:
+                    path[i][j] = grid[i][j]
+                    items[i][j] = [(i, j)]
+                elif i == 0:
                     path[i][j] = grid[i][j] + path[i][j - 1]
+                    items[i][j] = items[i][j - 1] + [(i, j)]
+                elif j == 0:
+                    path[i][j] = grid[i][j] + path[i - 1][j]
+                    items[i][j] = items[i - 1][j] + [(i, j)]
+                elif i == len(grid) - 1 and j == 0:
+                    path[i][j] = grid[i][j] + path[i - 1][j]
+                    items[i][j] = items[i - 1][j] + [(i, j)]
+                elif i == 0 and j == len(grid[i]) - 1:
+                    path[i][j] = grid[i][j] + path[i][j - 1]
+                    items[i][j] = items[i][j - 1] + [(i, j)]
+                else:
+                    if path[i - 1][j] > path[i][j - 1]:
+                        path[i][j] = grid[i][j] + path[i - 1][j]
+                        items[i][j] = items[i - 1][j] + [(i, j)]
+                    else:
+                        path[i][j] = grid[i][j] + path[i][j - 1]
+                        items[i][j] = items[i][j - 1] + [(i, j)]
 
-    return path
+        after_path.append(path[-1][-1])
+        three_paths.append(items[-1][-1])
+        
+        for (p1, p2) in items[-1][-1]:
+            grid[p1][p2] = 0
+        print(grid)
+
+    return sum(after_path), three_paths
 
 
 def main():
